d7746094e975c2d6a2e74245b23b80fc4fadb494
Landung Setiawan <landungs@uw.edu>
2019-12-06 16:02:42 -0800

    Make metadata use CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 
 
 def create_app(test_config=None):
     app = Flask("cava-metadata-api", instance_relative_config=True)
+    CORS(app)
     app.config.from_mapping(
         SECRET_KEY="dev", DATABASE=os.path.join("/opt/app", "CAVA_Assets.db")
     )
